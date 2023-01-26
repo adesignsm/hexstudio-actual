ffe62bce39727c2b2adf156077d1ef3d9de5d1fe
Akash <adesigns>
2023-01-26 12:49:05 -0500

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  console.log(children);
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
       <body>{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,90 +2,68 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
 
+import hashtag from "../../public/assets/hashtag.png";
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main id="main-home" className={styles.main}>
+      <div className={styles.vertical_line}></div>
+      <div className={styles.vertical_line}></div>
+      <article className={styles.article}>
+        <section className={styles.mission_container}>
+            <h1>Hex Studio</h1>
+            <h5>is a niche group of creatives rooted in creative problem solving & built on collaboration.
+                We impact our locals, modern brands, and individuals by providing holistic solutions thorugh
+                strategy, direction, design, and development.
+            </h5>
+        </section>
+      </article>
+      <article className={styles.article}>
+        <Image className={styles.hashtag_logo} src={hashtag} alt="hexstudio logo" priority/>
+      </article>
+      <nav>
+        <ul className={styles.ul}>
+          <li className={styles.li}>
+            <div>
+              <h1>Generative</h1>
+            </div>
+          </li>
+          <li className={styles.li}>
+            <div>
+              <h1>Services</h1>
+              <article>
+                <ul className={styles.services_list}>
+                  <li>Web Development</li>
+                  <li>Digital Experiences</li>
+                  <li>UI/UX Design</li>
+                  <li>Graphic Design</li>
+                  <li>Consultations</li>
+                </ul>
+              </article>
+            </div>
+          </li>
+          <li className={styles.li}>
+            <div>
+              <h1>Work</h1>
+            </div>
+          </li>
+          <li className={styles.li}>
+            <div className={styles.about_container}>
+              <h1>About</h1>
+              <section>
+                <h5>
+                  To use the power of design to help our clients communicate their ideas. values, and products
+                  in a way that is visually stunning and effective. We believe that design has the ability to shape the world around us,
+                  and we strive to create work that is innovative, meaningful, and impactful. Our ultimate goal is to help
+                  our clients succeed and make a positive difference in the world through digital innovation. 
+                </h5>
+              </section>
+            </div>
+          </li>
+        </ul>
+      </nav>
     </main>
   )
 }
